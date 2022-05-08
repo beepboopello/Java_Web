f6d23844cd7597d5d3da6aed238858244265be72
HueNguyen307 <93471792+HueNguyen307@users.noreply.github.com>
2022-05-08 15:46:20 +0700

    Jsps

--- src/java/controller/AdminServlet.java
+++ src/java/controller/AdminServlet.java
@@ -33,6 +33,7 @@ public class AdminServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         
+        request.getRequestDispatcher("admin.jsp").forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
